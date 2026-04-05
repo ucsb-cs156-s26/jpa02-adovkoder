8807126ae7c6b7b757fa28bf0a664f293d6fd6f9
Wilson Lee <wilsonlee@100-64-19-72.wireless.ucsb.edu>
2026-04-04 22:46:38 -0700

    wl - added tests for equals and hash

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,77 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_same_object() {
+
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_returns_different_types() {
+
+        int team2 = 1;
+
+        assertEquals(false, team.equals(team2));
+        
+    }
+
+    @Test
+    public void equals_returns_matching() {
+
+        Team team2 = new Team("test-team");
+
+        assertEquals(true, team.equals(team2));
+        
+    }
+
+    @Test
+    public void equals_returns_unmatching_members() {
+
+        Team team2 = new Team("test-team");
+        team2.addMember("Wilson");
+
+        assertEquals(false, team.equals(team2));
+        
+    }
+
+    @Test
+    public void equals_returns_unmatching_team() {
+
+        Team team2 = new Team("test-team2");
+
+        assertEquals(false, team.equals(team2));
+        
+    }
+
+    @Test
+    public void equals_returns_unmatching() {
+
+        Team team2 = new Team("test-team2");
+        team2.addMember("Wilson");
+
+        assertEquals(false, team.equals(team2));
+        
+    }
+
+    @Test
+    public void hashCode_returns_same_hash_value() {
+
+        Team team2 = new Team("test-team");
+
+        assertEquals(team.hashCode(), team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_correct_value() {
+
+        int result = team.hashCode();
+        assertEquals(-1226298695, result);
+
+    }
 }
